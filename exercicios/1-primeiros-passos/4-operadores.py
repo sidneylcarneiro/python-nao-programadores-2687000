2c2a9e5d849e94a87887100e839ff4be1a1c62ee
sidneylcarneiro <sidneylcarneiro@yahoo.com>
2024-06-09 12:22:37 +0000

    utilizando operadores com variaveis

--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -1,10 +1,24 @@
 ano_nascimento = 1989
 ano_formatura = 2010
-
+print("###################################")
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
-
-
+print("Idade de Gerlaine:", 2024-ano_nascimento)
+print("Formatura em:", ano_formatura)
+print("###################################")
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
-
-
+print(type(ano_nascimento))
+print(isinstance(ano_nascimento,int))
+print("###################################")
+print("ano_nascimento > ano_formatura:",ano_nascimento > ano_formatura)
+print("ano_nascimento < ano_formatura:",ano_nascimento < ano_formatura)
+print("ano_nascimento == ano_formatura:",ano_nascimento == ano_formatura)
+print("ano_nascimento >= ano_formatura:",ano_nascimento >= ano_formatura)
+print("ano_nascimento <= ano_formatura:",ano_nascimento <= ano_formatura)
+print("###################################")
 # Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+print("ano_nascimento > ano_formatura e ano_nascimento < ano_formatura:",ano_nascimento > ano_formatura and ano_nascimento < ano_formatura)
+print("ano_nascimento < ano_formatura or ano_nascimento == ano_formatura:",ano_nascimento < ano_formatura or ano_nascimento == ano_formatura)
+print("ano_nascimento == ano_formatura:",ano_nascimento == ano_formatura)
+print("ano_nascimento >= ano_formatura:",ano_nascimento >= ano_formatura)
+print("ano_nascimento <= ano_formatura:",ano_nascimento <= ano_formatura)
+print("not ano_nascimento <= ano_formatura:",not ano_nascimento <= ano_formatura)
