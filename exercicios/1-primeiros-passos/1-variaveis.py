7eb301abff4111dc8cb4e91edfba9fcef043a2db
sidneylcarneiro <sidneylcarneiro@yahoo.com>
2024-06-09 10:40:05 +0000

    Declare variáveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,11 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = "Meu Nome"
 # Declare variáveis de cada tipo numérico
-
-
+ano = 2024
+pi = 3.14159265359
 # Declare uma variável do tipo booleano
+ano2023 = False
+ano2024 = True
+
