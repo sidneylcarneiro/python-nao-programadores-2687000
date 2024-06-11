ad5d59a1d05cc4d6bde1f74ef63807ed0a26de07
sidneylcarneiro <sidneylcarneiro@yahoo.com>
2024-06-11 16:49:35 +0000

    #

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -1,5 +1,6 @@
 # Nesse desafio você verificará dentro de uma lista se o item estar contido nela, caso verdadeiro deverá imprimir na tela essa informação, além disso deverá solicitar avaliação para o item e armazená-la em um dicionário.
 # 1. Crie uma lista com 5 diferentes cursos do LinkedIn Learning
+
 # 2. Crie 3 variáveis do tipo string e associe 1 curso a cada uma delas
 # 3. Crie um dicionário vazio para armazenar a nota do curso
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
