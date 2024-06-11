463be60b2f868c44c5f33ddcda166130ef73a417
sidneylcarneiro <sidneylcarneiro@yahoo.com>
2024-06-11 17:41:06 +0000

    Criando lista e biblioteca comparando dados de variavel dentro da lista

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -1,8 +1,20 @@
 # Nesse desafio você verificará dentro de uma lista se o item estar contido nela, caso verdadeiro deverá imprimir na tela essa informação, além disso deverá solicitar avaliação para o item e armazená-la em um dicionário.
 # 1. Crie uma lista com 5 diferentes cursos do LinkedIn Learning
+learning_cursos = {"c1":"Tecnologia da Informação", 
+                   "c2":"Marketing", 
+                   "c3":"Programação"}
 
 # 2. Crie 3 variáveis do tipo string e associe 1 curso a cada uma delas
+Curso1 = "Tecnologia da Informação"
+Curso2 = "Marketing"
+Curso3 = "Programação"
+
 # 3. Crie um dicionário vazio para armazenar a nota do curso
+notas = {}
+
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
+print(learning_cursos)
+print(learning_cursos)
+
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
